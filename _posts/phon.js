77f091a4a9b66b198f2395adf09102504b62a751
mrgrammar <82765889+mrgrammar@users.noreply.github.com>
2021-04-18 10:50:00 -0500

    Add files via upload

---
+++ _posts/phon.js
@@ -0,0 +1,45 @@
+let pgames0 = [{
+//1 3137,2611,2856,723,1186,1186
+"diff": "guess",
+"wcount": 4,
+"choices": ["blue","boots","fruit","guess","move"],
+"answers": ["bluː","buːts","fruːt","ges","muːv"],
+"audios": ["w0400c","w1725b","w1430a","w0635a","w1551c"],
+"parts": ["aqua","blue","blue","red","red"]
+},
+{
+//2 1173,501,1811,279,3266,279
+"diff": "friend",
+"wcount": 4,
+"choices": ["agree","briefly","sea","friend","teacher"],
+"answers": ["əˈgriː","briːf.li","siː","frend","tiː.tʃər"],
+"audios": ["w0630c","w0246c","w2116c","w0132c","w1808b"],
+"parts": ["red","red_dashed","blue","blue","blue"]
+},
+{
+ // 3 1672,3619,543,355,1088,355
+"diff": "marry",
+"wcount": 4,
+"choices": ["car","farm","large","marry","smart"],
+"answers": ["kɑːr","fɑːrm","lɑːrdʒ","ˈmer.i","smɑːrt"],
+"audios": ["w0917a","w2006a","w0300c","w0158a","w0602b"],
+"parts": ["blue","blue","aqua","red","aqua"]
+},
+{
+ //4 2978,2783,3739,1508,487,1508
+"diff": "eyes",
+"wcount": 4,
+"choices": ["bell","desk","egg","eyes","never"],
+"answers": ["bel","desk","eg","aɪz","ˈnev.ər"],
+"audios": ["w1632b","w1527b","w2046a","w0822b","w0242a"],
+"parts": ["blue","blue","blue","blue","red_dashed"]
+},
+{
+//5 update 2158,2613,3472,1049,2788,1049
+"diff": "college",
+"wcount": 4,
+"choices": ["about","banana","college","police","polite"],
+"answers": ["əˈbaʊt","bəˈnæ.nə","ˈkɑː.lɪdʒ","pəˈliːs","pəˈlaɪt"],
+"audios": ["w1159a","w1430c","w1811c","w1917a","w0114a"],
+"parts": ["black","blue","blue","blue","blue"]
+}]
